00f3fa189711723fbd63e7d60f54e230a17bc844
Thekeebper <surapa.201041@gmail.com>
2021-12-17 12:30:03 +0700

    test_1

--- src/App.js
+++ src/App.js
@@ -1,22 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [data, setdata] = useState({
+    xovis: {
+      fw: "",
+      bw: "",
+    },
+  });
+
+  useEffect(() => {
+    async function fetchMyAPI() {
+      await axios({
+        method: "get",
+        url: `http://44.44.78.238:8085/get-data-2`,
+      })
+        .then(function (response) {
+          setdata({
+            xovis: {
+              fw: response.data["0"]["value"],
+              bw: response.data["1"]["value"],
+            },
+          });
+          console.log(data.xovis);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    }
+
+    fetchMyAPI();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>
+          <b>ทดสอบจำนวนครั้ง Sensor กล้อง Xovis การเข้าและออก</b>
+        </h1>
+        <div>
+          <span style={{ color: "green" }}>
+            ไปข้างหน้า : {data.xovis.fw} ครั้ง |{" "}
+          </span>
+          <span style={{ color: "red" }}>
+            ย้อนกลับมา : {data.xovis.bw} ครั้ง{" "}
+          </span>
+        </div>
       </header>
     </div>
   );
